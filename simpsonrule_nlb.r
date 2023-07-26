52ee84cf2991c24702057b1a3d5086b39c68725a
abcnorio <resets. 20280738+abcnorio@users.noreply.github.com>
2023-07-26 10:07:36 +0200

    bugfixes

--- simpsonrule_nlb.r
+++ simpsonrule_nlb.r
@@ -1,15 +1,15 @@
-#########################################################################
-# numerical integration
-# simple Simpson rule for
-# normal, log, brob functions ie. output
-simpsonrule.nlb <- function(fx, lower, upper,
-                	        type="normal", # log, brob
-	                        xmin=.Machine$double.xmin, #xmin=.Machine$double.neg.eps,                            
-	                        xmax=.Machine$double.xmax, #xmax=.Machine$double.eps,
-	                        log1p.crit=1e-09, #log1p.crit=1e-15,
-	                        Nsteps=100,
-		                    parallel=FALSE,
-	                        fixINF=TRUE, ...)
+
+################################################################################
+# Simpson rule for normal, log, and brob
+simpsonrule.nlb <-  function(fx,
+                             lower, upper,
+                             type="normal", # log, brob
+                             eps=.Machine$double.xmin,
+                             #eps=.Machine$double.eps,
+                             log1p.crit=1e-09, # 1e-15
+                             Nsteps=100,
+                             parallel=FALSE,
+                             fixINF=TRUE, ...)
 {
   
   if(lower == upper)
@@ -20,45 +20,43 @@ simpsonrule.nlb <- function(fx, lower, upper,
   {
     cat("\nBe aware of the limits chosen:\tlower > upper\n.Negative value of the integral will result.\n")
     return( -1 * simpsonrule.nlb(fx,
-                                  upper=upper, lower=lower,
-                                  type=type,
-                                  xmin=xmin,
-                                  xmax=xmax,
-                                  log1p.crit=log1p.crit,
-                                  Nsteps=Nsteps,
-                                  parallel=parallel,
-                                  fixINF=fixINF) )
+                                 upper, lower,
+                                 type=type,
+                                 eps=eps,
+                                 log1p.crit=log1p.crit,
+                                 Nsteps=Nsteps,
+                                 parallel=parallel,
+                                 fixINF=fixINF) )
   }    
   
   sek.l <- 2*Nsteps+1
   hml.s <- sign(upper-lower)
   
+  # avoid infinite values (zero with log(0), etc.)
+  if(fixINF == TRUE)
+  {
+    if(type %in% c("normal","log"))
+    {
+      if(is.infinite(fx(lower))) lower <- lower + eps * hml.s
+      if(is.infinite(fx(upper))) upper <- upper - eps * hml.s      
+    } else if(type == "brob")
+    {
+      if(is.infinite(fx(lower)@x)) lower <- lower + eps * hml.s
+      if(is.infinite(fx(upper)@x)) upper <- upper - eps * hml.s
+    }
+  }
+  sek <- seq(lower,upper,length=sek.l)
+  
   if(type == "normal")
   {
-    if(fixINF)
-    {
-      # avoid infinite values (zero with log(0), etc.)
-      if(is.infinite(fx(lower))) lower <- lower + xmin * hml.s
-      if(is.infinite(fx(upper))) upper <- upper - xmax * hml.s
-    }
-    sek <- seq(lower,upper,length=sek.l)
     xfxap <- fx(sek)
     h <- xfxap[2] - xfxap[1]
-    return( sum(h*( xfxap[2 * (1:Nsteps) - 1] +
+    res <- sum(h*( xfxap[2 * (1:Nsteps) - 1] +
                      4*xfxap[2 * (1:Nsteps)] +
                      xfxap[2 * (1:Nsteps) + 1] )
-               /3) )
-    
+               /3)
   } else if(type == "log")
   {
-    if(fixINF)
-    {
-      # avoid infinite values (zero with log(0), etc.)
-      if(is.infinite(fx(lower))) lower <- lower + xmin * hml.s
-      if(is.infinite(fx(upper))) upper <- upper - xmax * hml.s      
-    }
-    sek <- seq(lower,upper,length=sek.l)
-    
     if(parallel)
     {
       require(parallel)
@@ -78,31 +76,29 @@ simpsonrule.nlb <- function(fx, lower, upper,
     )
     max.s.log <- max(s.log)
     sum.log <- sum(exp(s.log - max.s.log))
-
+    #res <- h.log - log(3) + max.s.log + log1p(sum(exp(s.log[-which.max(s.log)] - max.s.log)))
+    #
     # when do log1p(x) vs. log(x)
     # https://scicomp.stackexchange.com/questions/20629/when-should-log1p-and-expm1-be-used
+    # IF 1+x=1 in floating point accuracy
+    # in case of x = 1e-15 which is -34.53878 on log scale
+    # ie. usual double precision arithmetic
+    # if is already useful for
     # x = 1e−09 which is -20.72327 on log scale
     ifelse(sum.log < log1p.crit,
-           return( h.log - log(3) +  max.s.log + log1p(sum.log) ),
-           return( h.log - log(3) +  max.s.log + log(sum.log) )
+           res <- h.log - log(3) +  max.s.log + log1p(sum.log),
+           res <- h.log - log(3) +  max.s.log + log(sum.log)
     )
     
   } else if(type=="brob")
   {
-    if(fixINF)
-    {
-      # avoid infinite values (zero with log(0), etc.)   
-      if(is.infinite(fx(lower)@x)) lower <- lower + xmin * hml.s
-      if(is.infinite(fx(upper)@x)) upper <- upper - xmax * hml.s      
-    }
-    sek <- seq(lower, upper, length.out=sek.l)
     fx.brob <- list2vec.brob(lapply(seq_along(1:sek.l), function(i) fx(sek[i])))
-    h <- as.brob(sek[2] - sek[1])   	
-    return( sum(h/as.brob(3) * ( fx.brob[2 * (1:Nsteps) - 1] +
-                            	               4 * fx.brob[2 * (1:Nsteps)] +
-                                             fx.brob[2 * (1:Nsteps) + 1]
-    ) ) )
+    h <- as.brob(sek[2] - sek[1]) # diff
+    res <- sum(h/as.brob(3) * ( fx.brob[2 * (1:Nsteps) - 1] +
+                                  4 * fx.brob[2 * (1:Nsteps)] +
+                                  fx.brob[2 * (1:Nsteps) + 1]
+    ) )
   }
-
+  return(res)
 }
-#########################################################################
+################################################################################
